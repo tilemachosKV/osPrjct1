5591d4d6be3dbb6457a2cbc43b458bc59abcd3c4
Tilemachos2003 <machoskostakis@gmail.com>
2025-11-22 18:44:00 +0200

    shared memory structure created

--- chat.c
+++ chat.c
@@ -1,133 +1,156 @@
-#include <stdio.h>           // Standard I/O: Used for printf, scanf, and perror
-#include <stdlib.h>          // Standard Library: Used for exit()
-#include <string.h>          // String Operations: Used for strcpy, strncmp
-#include <fcntl.h>           // File Control: Defines O_CREAT, O_RDWR constants
-#include <sys/stat.h>        // File Status: Defines modes/permissions (for 0666)
-#include <sys/mman.h>        // Memory Management: shm_open, mmap, munmap, MAP_SHARED
-#include <unistd.h>          // Unix Standard: ftruncate, close
+#include <stdio.h>     // Standard I/O: Used for printf, scanf, and perror
+#include <stdlib.h>    // Standard Library: Used for exit()
+#include <string.h>    // String Operations: Used for strcpy, strncmp
+#include <fcntl.h>     // File Control: Defines O_CREAT, O_RDWR constants
+#include <sys/stat.h>  // File Status: Defines modes/permissions (for 0666)
+#include <sys/mman.h>  // Memory Management: shm_open, mmap, munmap, MAP_SHARED
+#include <sys/types.h> // Required for pid_t definition
+#include <unistd.h>    // Unix Standard: ftruncate, close
 #include "sharedMemory.h"
 
 // checks whether the user input is correct
-int check_input(int argc, char* argv[], const char *params[]) {
-  if (argc != 4) return 0;
-  if (strcmp(argv[1], params[0]) != 0 && strcmp(argv[1], params[1]) != 0) return 0;
-  if (strcmp(argv[2], params[2]) != 0) return 0;
+int check_input(int argc, char *argv[], const char *params[])
+{
+  if (argc != 4)
+    return 0;
+  if (strcmp(argv[1], params[0]) != 0 && strcmp(argv[1], params[1]) != 0)
+    return 0;
+  if (strcmp(argv[2], params[2]) != 0)
+    return 0;
 
   return 1;
 }
 
-void init(int argc, char* argv[]) {
-  const char* name = "my_shm"; // Name of the shared memory object
-  const int SIZE = 4096;      // Size of the shared memory
-  char message[128];
+void init(int argc, char *argv[])
+{
+  const char *name = argv[3];
+  char user_input[128];
 
-  int shm_fd;   // Shared memory file descriptor
-  void* ptr;    // Pointer to the shared memory
+  int shm_fd; // Shared memory file descriptor
+  void *ptr;  // Pointer to the shared memory
 
-  SharedData* someMemory;
+  SharedMemory *someMemory;
 
   // 1. Create the shared memory object
   shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-  if (shm_fd == -1) {
+  if (shm_fd == -1)
+  {
     perror("shm_open");
     exit(1);
   }
 
-  // 2. Set the size of the shared memory
-  ftruncate(shm_fd, SIZE);
+  // 2. Set the SHM_SIZE of the shared memory
+  ftruncate(shm_fd, SHM_SIZE);
 
   // 3. Map the shared memory object into our address space
-  ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-  if (ptr == MAP_FAILED) {
+  ptr = mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (ptr == MAP_FAILED)
+  {
     perror("mmap");
     exit(1);
   }
 
-  someMemory = (SharedData*)ptr;
-  sem_init(&someMemory->newMessage, 1, 0);
+  someMemory = (SharedMemory *)ptr;
+  sem_init(&someMemory->shm_sem, 1, 0);
+  someMemory->active_dialogues_count++;
+
+  // Init dialoge
+  someMemory->dialogues[0].dialogue_id = someMemory->active_dialogues_count;
+  someMemory->dialogues[0].num_active_participants = 1;
+  sem_init(&someMemory->dialogues[0].message.access, 1, 0);
+
+  // Init message
+  someMemory->dialogues[0].message.sender_pid = getpid();
 
   // 4. Write to the shared memory
-  // printf("Writing to shared memory: '%s'\n", message);
   printf("Enter messages to write (type 'DONE' to quit):\n");
-  while (strncmp(message, "DONE", 5) != 0) { // Use 5 to check "DONE\0"
+  while (strncmp(user_input, "DONE", 5) != 0)
+  { // Use 5 to check "DONE\0"
     printf("NEW MESSAGE: ");
     fflush(stdout);
 
-    int scan_result = scanf("%127s", message);
-    if (scan_result != 1) {
+    int scan_result = scanf("%127s", user_input);
+    if (scan_result != 1)
+    {
       // Handle End-of-File (Ctrl+D) or read error
-      strcpy(someMemory->data, "DONE"); // Write DONE as last message
-      sem_post(&someMemory->newMessage);
       break;
     }
 
-    strcpy(someMemory->data, message);
-    sem_post(&someMemory->newMessage);
-  } 
-
-  printf("Exiting.\n");
+    strcpy(someMemory->dialogues[0].message.text, user_input);
+    someMemory->dialogues[0].message.times_read = 0;
+    sem_post(&someMemory->dialogues[0].message.access);
+  }
 
   // 5. Unmap the shared memory
-  munmap(ptr, SIZE);
-  
+  munmap(ptr, SHM_SIZE);
+
   // 6. Close the file descriptor
   close(shm_fd);
 }
 
-void attach(int argc, char* argv[]) {
-  const char* name = "my_shm";
-  const int SIZE = 4096;
-  int shm_fd;
-  void* ptr;
+void attach(int argc, char *argv[])
+{
+  const char *name = argv[3];
 
-  SharedData* someMemory;
+  int shm_fd;
+  void *ptr;
+
+  SharedMemory *someMemory;
 
   // 1. Open the shared memory object
   shm_fd = shm_open(name, O_RDWR, 0666);
-  if (shm_fd == -1) {
-    perror("shm_open"); 
+  if (shm_fd == -1)
+  {
+    perror("shm_open");
     exit(1);
   }
 
   // 2. Map the shared memory object
-  ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-  if (ptr == MAP_FAILED) {
+  ptr = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (ptr == MAP_FAILED)
+  {
     perror("mmap");
     exit(1);
   }
 
-  someMemory = (SharedData*)ptr;
+  someMemory = (SharedMemory *)ptr;
 
   // 3. Read from the shared memory
-  while (strncmp(someMemory->data, "DONE", 4) != 0) {
-    sem_wait(&someMemory->newMessage);
-    printf("MESSAGE: '%s'\n", someMemory->data);
+  while (strncmp(someMemory->dialogues[0].message.text, "DONE", 4) != 0)
+  {
+    sem_wait(&someMemory->dialogues[0].message.access);
+    printf("MESSAGE: '%s'\n", someMemory->dialogues[0].message.text);
   }
-  printf("Last read from shared memory: '%s'\n", someMemory->data);
+  printf("Last read from shared memory: '%s'\n", someMemory->dialogues[0].message.text);
 
   // 4. Unmap the shared memory
-  munmap(ptr, SIZE);
+  munmap(ptr, SHM_SIZE);
 
   // 5. Close the file descriptor
   close(shm_fd);
-  
+
   // 6. Remove the shared memory object
   // Only the reader (or a dedicated cleanup process) should do this
   shm_unlink(name);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   const char *params[] = {"-i", "-a", "-k"};
 
-  if (!check_input(argc, argv, params)) {
+  if (!check_input(argc, argv, params))
+  {
     printf("WRONG INPUT\nTry the bellow:\n./chat -i -k your_key   OR   ./chat -a -k your_key\n");
     printf("Exiting...\n");
     return -1;
   }
 
-  if (strcmp(argv[1], params[0]) == 0) {
+  if (strcmp(argv[1], params[0]) == 0)
+  {
     init(argc, argv);
-  }else {
+  }
+  else
+  {
     attach(argc, argv);
   }
 
